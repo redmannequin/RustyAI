bcfe12a02d994adde5ff12d72021f9857c95debf
flavio.b <barinas.flavio@gmail.com>
2018-02-15 01:05:16 -0500

    fixed a_star

--- rusty_ai/src/search/heuristic/a_star.rs
+++ rusty_ai/src/search/heuristic/a_star.rs
@@ -20,7 +20,10 @@ pub fn a_start<T>(start: T) -> Vec<T> where T:Hash+State+StateCost+Production<It
     while let Some(node) = queue.pop() {
 
         let node_id = node.get_id();
-        if visited.contains_key(&node_id) { continue; }
+        if visited.contains_key(&node_id) {
+            update_node(&mut queue,&mut visited, node);
+            continue;
+        }
         
         match node.get_state() {
             Dead => continue,
@@ -32,9 +35,11 @@ pub fn a_start<T>(start: T) -> Vec<T> where T:Hash+State+StateCost+Production<It
         
         for mut neighbor_node in node.production() {
             let neighbor_id = neighbor_node.get_id();
+            neighbor_node.add_parent(node_id);
             if !visited.contains_key(&neighbor_id) {
-                neighbor_node.add_parent(node_id);
                 queue.push(neighbor_node);
+            } else {
+                update_node(&mut queue, &mut visited, neighbor_node);
             }
         }
 
@@ -42,4 +47,15 @@ pub fn a_start<T>(start: T) -> Vec<T> where T:Hash+State+StateCost+Production<It
     }
     
     return get_path(goal_node, &mut visited);
+}
+
+fn update_node<T>(queue: &mut BinaryHeap<Node<T>>, tree: &mut HashMap<u64,Node<T>>, node: Node<T>) where T:Hash+StateCost {
+    let node_id = node.get_id();
+    let old_node = tree.remove(&node_id).unwrap();
+
+    if old_node > node {
+        queue.push(node);
+    } else {
+        tree.insert(node_id, old_node);
+    }
 }
