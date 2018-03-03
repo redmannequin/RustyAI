fc90006b75d9c0bca2d25be6b6cb89e839c081c1
flavio.b <barinas.flavio@gmail.com>
2018-03-02 20:01:39 -0500

    small clean up + template for minimax

--- rusty_ai/src/search/heuristic/a_star.rs
+++ rusty_ai/src/search/heuristic/a_star.rs
@@ -1,8 +1,7 @@
 use std::hash::Hash;
-use std::collections::HashSet;
 use std::collections::HashMap;
 use std::collections::BinaryHeap;
-use std::cmp::Ordering;
+
 
 use core::node::Node;
 use core::state::State;
@@ -11,13 +10,14 @@ use core::state::Production;
 use core::state::StateType;
 
 use search::get_path;
+use search::heuristic::Score;
 
 pub fn a_star<T>(start: T) -> Vec<T> where T:Clone+Hash+State+StateCost+Production<Item=T> {
     
     let mut queue: BinaryHeap<Score> = BinaryHeap::new();
     let mut scores: HashMap<u64,f32> = HashMap::new();
-    let mut openSet: HashMap<u64,Node<T>> = HashMap::new();
-    let mut closedSet: HashMap<u64,Node<T>> = HashMap::new();
+    let mut open_set: HashMap<u64,Node<T>> = HashMap::new();
+    let mut closed_set: HashMap<u64,Node<T>> = HashMap::new();
 
     let mut goal_node: Option<Node<T>> = None;
 
@@ -27,20 +27,20 @@ pub fn a_star<T>(start: T) -> Vec<T> where T:Clone+Hash+State+StateCost+Producti
 
     queue.push(start_score.clone());
     scores.insert(start_node_id, start_score.score);
-    openSet.insert(start_node_id, start_node);
+    open_set.insert(start_node_id, start_node);
     
     while let Some(score) = queue.pop() {
         let node_id = score.id;
 
         if scores.len()%100 == 0 {
-            println!("openset: {} | Q: {} | closeset: {} | visited: {} | D: {}", openSet.len(), queue.len(), closedSet.len(), scores.len(), score.score);
+            println!("openSet: {} | Q: {} | closeset: {} | visited: {} | D: {}", open_set.len(), queue.len(), closed_set.len(), scores.len(), score.score);
         }
         
-        if let Some(node) = openSet.remove(&node_id) {
+        if let Some(node) = open_set.remove(&node_id) {
             match node.get_state() {
                 StateType::Live => {
-                    closedSet.insert(node_id, node.clone());
-                    add_neighbors(&mut queue, &mut scores, &mut openSet, &closedSet, node);  
+                    closed_set.insert(node_id, node.clone());
+                    add_neighbors(&mut queue, &mut scores, &mut open_set, &closed_set, node);  
                 },
                 StateType::Goal => {
                     goal_node = Some(node);
@@ -50,14 +50,14 @@ pub fn a_star<T>(start: T) -> Vec<T> where T:Clone+Hash+State+StateCost+Producti
             }
         }
     }
-    return get_path(goal_node, &mut closedSet);
+    return get_path(goal_node, &mut closed_set);
 }
 
 fn add_neighbors<T> (
     queue: &mut BinaryHeap<Score>, 
     scores: &mut HashMap<u64,f32>, 
-    openSet: &mut HashMap<u64,Node<T>>, 
-    closedSet: &HashMap<u64,Node<T>>, 
+    open_set: &mut HashMap<u64,Node<T>>, 
+    closed_set: &HashMap<u64,Node<T>>, 
     node: Node<T>
 ) where T:Clone+Hash+State+StateCost+Production<Item=T> {
     
@@ -67,11 +67,11 @@ fn add_neighbors<T> (
         let neighbor_id = neighbor_node.get_id();
         neighbor_node.add_parent(node_id);
 
-        if closedSet.contains_key(&neighbor_id) { continue; }
+        if closed_set.contains_key(&neighbor_id) { continue; }
 
         let mut score = Score::new(&neighbor_node);
 
-        if openSet.contains_key(&neighbor_id) {
+        if open_set.contains_key(&neighbor_id) {
             let old_score = scores.get(&neighbor_id).unwrap().clone();
             if score.score + node_score > old_score {
                 continue;
@@ -80,49 +80,6 @@ fn add_neighbors<T> (
 
         queue.push(score.clone());
         scores.insert(neighbor_id, score.score + node_score);
-        openSet.insert(neighbor_id, neighbor_node);
+        open_set.insert(neighbor_id, neighbor_node);
     }
 }
-
-
-// SCORE
-#[derive(Clone)]
-struct Score {
-    id: u64,
-    score: f32,
-}
-
-impl Score {
-
-    fn new<T>(node: &Node<T>) -> Self where T:Hash+State+StateCost {
-        let id = node.get_id();
-        let score = node.get_score() + node.get_heuristic();
-        return Score {
-            id: id,
-            score: score,
-        };
-    }
-}
-
-//
-impl PartialOrd for Score where {
-    fn partial_cmp(&self, other: &Score) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Score where {
-    fn cmp(&self, other: &Score) -> Ordering {
-        if self.score > other.score { return Ordering::Greater; } 
-        if self.score < other.score { return Ordering::Less; }
-        return Ordering::Equal;
-    }
-}
-
-impl PartialEq for Score where {
-    fn eq(&self, other: &Score) -> bool {
-        return self.score == other.score;
-    }
-}
-
-impl Eq for Score where {}
--- rusty_ai/src/search/heuristic/mod.rs
+++ rusty_ai/src/search/heuristic/mod.rs
@@ -1,2 +1,51 @@
+use std::hash::Hash;
+use std::cmp::Ordering;
+
+use core::node::Node;
+use core::state::State;
+use core::state::StateCost;
 
 pub mod a_star;
+pub mod minimax;
+
+
+#[derive(Clone)]
+struct Score {
+    id: u64,
+    score: f32,
+}
+
+impl Score {
+
+    fn new<T>(node: &Node<T>) -> Self where T:Hash+State+StateCost {
+        let id = node.get_id();
+        let score = node.get_score() + node.get_heuristic();
+        return Score {
+            id: id,
+            score: score,
+        };
+    }
+}
+
+//
+impl PartialOrd for Score where {
+    fn partial_cmp(&self, other: &Score) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Score where {
+    fn cmp(&self, other: &Score) -> Ordering {
+        if self.score > other.score { return Ordering::Greater; } 
+        if self.score < other.score { return Ordering::Less; }
+        return Ordering::Equal;
+    }
+}
+
+impl PartialEq for Score where {
+    fn eq(&self, other: &Score) -> bool {
+        return self.score == other.score;
+    }
+}
+
+impl Eq for Score where {}
