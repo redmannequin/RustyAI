957483af54200386988ab3dae71cf43ec405ba53
flavio.b <barinas.flavio@gmail.com>
2018-03-11 15:46:02 -0400

    small error

--- rusty_ai/src/core/env.rs
+++ rusty_ai/src/core/env.rs
@@ -3,7 +3,7 @@ use core::state::State;
 use core::state::Production;
 
 // Evn
-pub trait Env<T> where T:State+Production<Item=T>  {
+pub trait Env<T> where T:State {
     type Action;
     fn step(&mut self, action: Self::Action) -> T;
     fn get_action_space(&self) -> u8;
